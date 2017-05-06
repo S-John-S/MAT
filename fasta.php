cd03db95121ab6badb9324ed36d99f252e859c18
S-John-S <classworkpc@gmail.com>
2017-05-06 09:18:33 +0530

    Add files via upload

---
+++ fasta.php
@@ -0,0 +1,20 @@
+<?php   
+error_reporting(E_ALL);
+$output = output_file;
+
+$pdb_file = "inputfile.pdb";
+if ( isset($_POST['feature'][4]) )
+{
+	passthru("pdb_std -i $pdb_file -b > $output 2>&1");
+	$output_file = fopen(getcwd()."/".$output, "r");
+	$content = fread($output_file, filesize(getcwd()."/".$output));
+	$lines = explode("\n", $content);
+	fclose($output_file);
+        foreach ( $lines as $line ) {
+        	echo $line . "<br/>";
+        	}
+ }
+    else {
+	echo "N/A";
+}
+ ?> 
