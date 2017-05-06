f32e02014d759065400e89023570953ef8af4ab1
S-John-S <classworkpc@gmail.com>
2017-05-06 07:51:16 +0530

    Add files via upload

---
+++ result.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="public/images/logotemp.png" rel="shortcut icon">
+    <title>MAT</title>
+<link rel="stylesheet" href="public/css/bootstrap.min.css">
+<script src="public/scripts/jquery-3.1.1.min.js" type="text/javascript"></script>
+<script src="public/scripts/bootstrap.min.js" type="text/javascript"></script>
+<script src="public/scripts/ladda.min.js" type="text/javascript"></script>
+<script src="public/scripts/spin.min.js" type="text/javascript"></script>
+<link rel="stylesheet" href="public/fonts/glyphicons-halflings-regular.eot">
+<link rel="stylesheet" href="public/fonts/glyphicons-halflings-regular.woff">
+<link rel="stylesheet" href="public/fonts/glyphicons-halflings-regular.woff2">
+<link rel="stylesheet" href="public/fonts/glyphicons-halflings-regular.ttf">
+<link rel="stylesheet" href="public/fonts/glyphicons-halflings-regular.svg">
+<link rel="stylesheet" href="styles.css">
+<link rel="stylesheet" href="public/css/ladda.min.css">
+</head>
+<body>
+<div id="navbar">
+  <nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+      <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      </button>
+      
+        
+        <a class="navbar-brand" href="index.html"><b>Home</b></a>
+    </div>
+    <div class="collapse navbar-collapse" id="navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active navbar-brand"><a href="mat.php" >MAT</a> </li>
+        <li ><a class="navbar-brand" href="#">Science</a></li>
+        <li ><a class="navbar-brand" href="documentation.html">Documentation</a></li>
+        <li><a class="navbar-brand" href="downloads.html">Downloads</a></li>
+        <li><a class="navbar-brand" href="#">Publication</a></li>
+        <li><a class="navbar-brand" href="contact.html">About Us</a></li>
+        <li><a class="navbar-brand" href="#">Links</a></li>    
+      </ul>
+    </div>
+      </div>
+    <!-- /.navbar-collapse -->
+  </nav>
+</div>
+
+<pre>      Salt Bridge      </pre>
+<div style="width:50%;height:200px;overflow:scroll;">
+
+</div>
+
+<hr style="color:white;border-style:inset;border-width:2px;margin-top:0.5em;margin-bottom:0.5em;">
+<pre>      Protein Slicer      </pre>
+<div style="width:50%;height:200px;overflow:scroll;">
+
+</div>
+
+<hr style="color:white;border-style:inset;border-width:2px;margin-top:0.5em;margin-bottom:0.5em;">
+<pre>      Metal Interaction      </pre>
+<div style="width:50%;height:200px;overflow:scroll;">
+
+</div>
+
+<hr style="color:white;border-style:inset;border-width:2px;margin-top:0.5em;margin-bottom:0.5em;">
+<pre>      Missing Residues      </pre>
+<div style="width:50%;height:200px;overflow:scroll;">
+
+</div>
+
+<hr style="color:white;border-style:inset;border-width:2px;margin-top:0.5em;margin-bottom:0.5em;">
+<pre>      PDB to Fasta      </pre>
+<div style="width:50%;height:200px;overflow:scroll;">
+
+</div>
+
+<hr style="color:white;border-style:inset;border-width:2px;margin-top:0.5em;margin-bottom:0.5em;">
+<pre>      Active Sites      </pre>
+<div style="width:50%;height:200px;overflow:scroll;">
+
+</div>
+
+<hr style="color:white;border-style:inset;border-width:2px;margin-top:0.5em;margin-bottom:0.5em;">
+ 
+    
+
+    
+    
+
+ <footer style="background-color:black;position:relative;bottom:0;width:100%;"> 
+     <p style="color:gray;text-align:center;font-size:12px;">©  Copyright 2015-2016, The MAT Project, Hazra Lab.</p>
+</footer>
+
+</body>
+</html>
+ 
