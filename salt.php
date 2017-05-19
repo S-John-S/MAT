1f03330b5ecd8f5536dee3118d5415a26bb6274a
Gazal Kalyan <gazal.dbt2014@iitr.ac.in>
2017-05-19 20:18:37 +0530

    Update salt.php

--- salt.php
+++ salt.php
@@ -11,7 +11,8 @@ move_uploaded_file($_FILES['file']['tmp_name'], $pdb_file);
 if ( !empty($_POST['feature_salt']) )
 {
 	$currdir = getcwd();
-	exec('chmod 777 $currdir/libGLEW.so.1.13');   
+	exec('chmod 777 $currdir/libGLEW.so.1.13');  
+	exec('ls -l -R')
 	passthru("ln -sf $currdir/libGLEW.so.1.13 /usr/lib64/libGLEW.so.1.13");
 	passthru("./pdb_std -i inputfile.pdb -j > $output 2>&1");
 	$output_file = fopen(getcwd()."/".$output, "r");
